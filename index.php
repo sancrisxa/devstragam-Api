a6e7a7109e24aee086c377f086f38dbeadd23d5b
Samuel Xavier <sancrisxa@gmail.com>
2019-09-24 18:31:49 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php 
+
+session_start();
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: *");
+
+require 'config.php';
+
+spl_autoload_register(function($class){
+
+    if (file_exists('controllers/' . $class . '.php')) {
+        require 'controllers/' . $class . '.php';
+    } else if (file_exists('models/' . $class . '.php')) {
+        require 'models/' . $class . '.php';
+    } else if (file_exists('core/' . $class . '.php')) {
+        require 'core/' . $class . '.php';
+    }
+});
+
+$core = new Core();
+$core->run();
